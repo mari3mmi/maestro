2175bdd250f68312bd4c9faa05cb6d0d4da3845d
Amarisse <amarissemarie23@gmail.com>
2019-02-09 17:45:15 -0500

    adding node-sass and deleting extra files

---
+++ src/components/client/Client.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Client.scss';
+
+
+class Client extends Component {
+    render() {
+      return (
+        <div className="Client">
+          <header className="header">
+            <p>
+             Client Page
+            </p>
+          </header>
+        </div>
+      );
+    }
+  }
+  
+  export default Client;
---
+++ src/components/master/Master.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Master.scss';
+
+
+class Master extends Component {
+    render() {
+      return (
+        <div className="Master">
+          <header className="header">
+            <p>
+              Master Page
+            </p>
+          </header>
+        </div>
+      );
+    }
+  }
+  
+  export default Master;
